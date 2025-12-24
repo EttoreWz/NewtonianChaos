42eef2d8e55e1de777578eb6e4cd14d0054c2cb7
EttoreWz <ettorewoodsa@gmail.com>
2025-12-24 00:48:39 -0500

    Update Pipeline.java

--- Pipeline.java
+++ Pipeline.java
@@ -1,27 +1,39 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Pipeline {
-    public void uploadData(ArrayList<Integer> prngValues, ArrayList<Long> evenBits, ArrayList<Long> oddBits) {
-        String url = "jdbc:mysql://localhost:3306/testDB";
+	private static int index=1; 
+    public Pipeline(ArrayList<Integer> prngValues, Map<Integer, Long> evenBits, Map<Integer, Long> oddBits) {
+        String url = "jdbc:mysql://localhost:3306/newton";
         String user = "root";
         String password = "password";
         
-        String query = "INSERT INTO newtonianData (PRNG) VALUES (?)";
+        String query = "INSERT INTO newtonianData VALUES (?,?,?,?)";
 
         try (Connection conn = DriverManager.getConnection(url, user, password);
              PreparedStatement pstmt = conn.prepareStatement(query)) {
-
-            for (Integer value : prngValues) {
-                pstmt.setInt(1, value);
-                pstmt.addBatch();       
+        	
+        	int size = prngValues.size();
+            for (int i=0; i<size; i++) {
+            	pstmt.setInt(1,index);
+                pstmt.setInt(2, prngValues.get(i));
+                if (evenBits.get(i) != null) {
+                    pstmt.setFloat(3, evenBits.get(i));
+                    pstmt.setFloat(4, oddBits.get(i));
+                } else {
+                    pstmt.setNull(3, java.sql.Types.FLOAT);;
+                    pstmt.setNull(4, java.sql.Types.FLOAT);;
+                }
+                pstmt.addBatch();
+                index++;
             }
 
             pstmt.executeBatch();
-            
+            System.out.println("Pipeline Transfer Complete.");
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
 }
